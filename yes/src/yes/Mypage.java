7323d628d42a793a7c305b9135e754c773f6e7c5
young jin Yun <yunyj1224@gmail.com>
2024-05-01 02:15:22 +0900

    홈,날짜선택,마이페이지 프레임 연결

--- yes/src/yes/Mypage.java
+++ yes/src/yes/Mypage.java
@@ -1,7 +1,7 @@
 package yes.src.yes;
 
 import java.awt.event.ActionListener;
-import java.awt.FlowLayout;
+
 import javax.swing.*;
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane.CloseAction;
 import java.awt.event.*;
@@ -15,65 +15,49 @@ import javax.swing.SwingUtilities;
 import javax.swing.JFrame;
 
 public class Mypage extends JFrame {
-	private JFrame previousFrame;
+   JFrame frame1 = this;
 
-	public Mypage(JFrame previousFrame) {
-		this.previousFrame = previousFrame;
+   public Mypage() {
+      
 
-		setTitle("마이 페이지");
-		setSize(1200, 800);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(new FlowLayout());
-		getContentPane().setLayout(null);
+      setTitle("마이 페이지");
+      setBounds(350,150,1200, 800);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setLayout(new FlowLayout());
+      getContentPane().setLayout(null);
 
-		// Back button
-		JButton btn1 = new JButton("영화현황");
-		JButton btn2 = new JButton("예매영화");
-		JButton backButton = new JButton("뒤로");
-		btn1.setBounds(1, 10, 1200, 100);
-		btn2.setBounds(1, 170, 122, 30);
-		backButton.setBounds(1, 580, 100, 30);
-		
-		getContentPane().add(btn1);
-		getContentPane().add(btn2);
-		run();
-		backButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// Close the current frame
-				dispose();
-				// Show the previous frame
-				previousFrame.setVisible(true);
-			}
-		});
-		
-		getContentPane().add(backButton, BorderLayout.CENTER);
-	}
-	
-	public void run() {
-		// First Frame
-		JFrame firstFrame = new JFrame("영화 예매 시스템");
-		firstFrame.setSize(1200, 800);
-		firstFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		firstFrame.setLayout(new FlowLayout());
-		firstFrame.setLayout(null);
-
-		// Button to open the second frame
-		JButton nextButton = new JButton("마이 페이지 ");
-		nextButton.setBounds(1000, 1, 200, 100);
-
-		nextButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// Close the current frame
-				firstFrame.setVisible(false);
-				// Open the second frame
-				JFrame secondFrame = new Mypage(firstFrame);
-				secondFrame.setVisible(true);
-			}
-		});
-
-		firstFrame.getContentPane().add(nextButton, BorderLayout.CENTER);
-		firstFrame.setVisible(true);
-	}
+      // Back button
+      JPanel Your_Book_Panel = new JPanel();
+      Your_Book_Panel.setBounds(0, 0, 1200, 150);
+      Your_Book_Panel.setLayout(null);
+      Your_Book_Panel.setBackground(Color.white);
+      
+      JLabel Your_Book = new JLabel("예매 현황");
+      Your_Book.setBounds(500, 0, 300, 150);
+      Font font = new Font("Malgun Gothic",Font.BOLD,30);
+      Your_Book.setFont(font);
+      Your_Book_Panel.add(Your_Book);
+      
+      JButton backButton = new JButton("홈");
+      backButton.setBounds(1, 580, 100, 30);
+      
+      
+      
+      backButton.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            Homedemo frame2 = new Homedemo();
+            frame1.dispose();
+            frame2.setVisible(true);
+            
+            
+         }
+      });
+      
+      getContentPane().add(backButton, BorderLayout.CENTER);
+      add(Your_Book_Panel);
+      setVisible(true);
+   }
+   
+  
 }
